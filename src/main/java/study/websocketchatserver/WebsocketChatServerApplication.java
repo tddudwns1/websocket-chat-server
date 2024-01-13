c6d0bc08f6e66dbb14f40e4260893abfd4924dba
tddudwns1 <tjddudwns1@naver.com>
2024-01-13 14:52:36 +0900

    [init] 프로젝트 생성
    1. 버전
    springboot: 3.2.1
    java: 17
    2. 의존성 추가
    spring web, websocket, lombok

---
+++ src/main/java/study/websocketchatserver/WebsocketChatServerApplication.java
@@ -0,0 +1,13 @@
+package study.websocketchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/websocketchatserver/WebsocketChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.websocketchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
