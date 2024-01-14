3c462b5d55436848de7bf22c4a9e61344c18bd5d
tddudwns1 <tjddudwns1@naver.com>
2024-01-14 12:15:47 +0900

    [feat] Redis 발행 서비스 구현
    채팅방에 입장하여 메시지를 작성하면 해당 메시지를 Redis Topic에 발행하는 기능의 서비스
    메시지를 발행하면 대기하고 있던 redis 구독 서비스가 메시지를 처리

---
+++ src/main/java/study/websocketchatserver/pubsub/RedisPublisher.java
@@ -0,0 +1,16 @@
+package study.websocketchatserver.pubsub;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+import study.websocketchatserver.model.ChatMessageDTO;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+    public void publish(ChannelTopic topic, ChatMessageDTO message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
