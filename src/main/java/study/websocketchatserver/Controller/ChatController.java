4e63df968ffb05a90e0f51fb413729063ea5ef31
tddudwns1 <tjddudwns1@naver.com>
2024-01-14 11:18:45 +0900

    [fix] Stomp로 채팅서버 고도화하기위한 기존 코드 수정
    1. WebSockConfig 수정
    Stomp를 사용하기 위해 @EnableWebSocketMessageBroker을 선언
    WebSocketMessageBrokerConfigurer를 상속받아 configureMessageBroker를 구현
    pub/sub 메시징을 구현
    - 메시지를 발행하는 요청의 prefix는 /pub로 시작하도록 설정
    - 메시지를 구독하는 요청의 prefix는 /sub로 시작하도록 설정
    stomp websocket의 연결 endpoint는 /ws-stomp로 설정
    2. 채팅 방 DTO 수정
    채팅방 DTO 간소화
    (pub/sub방식을 이용하면 구독자 관리가 알아서 되므로 웹소켓 세션 관리가 필요 x, 발송의 구현도 알아서 해결되므로 일일이 클라이언트에게 메시지를 발송하는 구현이 필요 x)
    3. ChatController수정 ( publisher 구현 )
    @MessageMapping을 통해 Websocket으로 들어오는 메시지 발행을 처리
    @MessageMapping을 통해 Websocket으로 들어오는 메시지 발행을 처리
    메시지가 발행되면 /sub/chat/room/{roomId}로 메시지를 send 하는 것을 볼 수 있는데 클라이언트에서는 해당 주소를(/sub/chat/room/{roomId}) 구독(subscribe)하고 있다가 메시지가 전달되면 화면에 출력
    /sub/chat/room/{roomId}는 채팅룸을 구분하는 값이므로 pub/sub에서 Topic의 역할

--- src/main/java/study/websocketchatserver/Controller/ChatController.java
+++ src/main/java/study/websocketchatserver/Controller/ChatController.java
@@ -1,25 +1,21 @@
 package study.websocketchatserver.Controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import study.websocketchatserver.model.ChatRoomDTO;
-import study.websocketchatserver.service.ChatService;
-
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import study.websocketchatserver.model.ChatMessageDTO;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
+//@RestController
+//@RequestMapping("/chat")
 public class ChatController {
-    private final ChatService chatService;
-
-    @PostMapping
-    public ChatRoomDTO createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoomDTO> findAllRoom() {
-        return chatService.findAllRoom();
+    private final SimpMessageSendingOperations messagingTemplate;
+    @MessageMapping("/chat/message")
+    public void message(ChatMessageDTO message) {
+        if (ChatMessageDTO.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
