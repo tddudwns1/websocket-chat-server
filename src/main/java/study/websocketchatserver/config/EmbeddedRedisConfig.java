70d54050c20b8dbb5661c2ca2788c88b6477636b
tddudwns1 <tjddudwns1@naver.com>
2024-01-14 11:56:18 +0900

    [feat] EmbeddedRedisConfig 생성
    1. Embedded Redis 서버 사용을 위한 설정
    채팅 서버가 실행될때 Embedded Redis 서버도 동시에 실행 됨
     local 환경에서만 실행되도록 @Profile(“local”)을 상단에 선언

---
+++ src/main/java/study/websocketchatserver/config/EmbeddedRedisConfig.java
@@ -0,0 +1,29 @@
+package study.websocketchatserver.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Profile("local")
+@Configuration
+public class EmbeddedRedisConfig {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
